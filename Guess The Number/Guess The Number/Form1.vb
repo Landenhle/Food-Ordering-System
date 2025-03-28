589fbf1840623972705d4e89db6f03e97eb91ca6
Landenhle Zigode <107732827+Landenhle@users.noreply.github.com>
2025-03-28 15:35:42 +0200

    Add files via upload

---
+++ Guess The Number/Guess The Number/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim correct As Integer = 8
+        If Textbox1.Text = correct Then
+            PictureBox1.Image = My.Resources.tick
+            PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        ElseIf TextBox1.Text < correct Then
+            PictureBox1.Image = My.Resources.uparrow
+            PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        Else
+            PictureBox1.Image = My.Resources.downarrow
+            PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        End If
+    End Sub
+End Class
